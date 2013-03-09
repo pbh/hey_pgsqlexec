e7c5ca340febed44db9179d001abbdb49ca7271d
Paul H <git@heymann.be>
2013-03-09 11:43:44 +0000

    minor naming change

--- hey_pgsqlexec/test/txt/check_append.sql
+++ hey_pgsqlexec/test/txt/check_append.sql
@@ -1,4 +1,4 @@
-CREATE TEMPORARY TABLE "holymoly_test_tab" (foo integer, bar integer);
-INSERT INTO "holymoly_test_tab" (foo, bar) VALUES (1,2);
-INSERT INTO "holymoly_test_tab" (foo, bar) VALUES (3,3);
-SELECT SUM(foo) FROM "holymoly_test_tab";
+CREATE TEMPORARY TABLE "pgsqlexec_test_tab" (foo integer, bar integer);
+INSERT INTO "pgsqlexec_test_tab" (foo, bar) VALUES (1,2);
+INSERT INTO "pgsqlexec_test_tab" (foo, bar) VALUES (3,3);
+SELECT SUM(foo) FROM "pgsqlexec_test_tab";

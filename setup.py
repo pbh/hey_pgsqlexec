8cd4a39d256b689b0df0c1252fac74917b2fc241
Paul H <git@heymann.be>
2013-03-10 02:32:57 +0000

    add manifest and url

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='1.0.2',
     author='Paul Heymann',
     author_email='hey_pgsqlexec@heymann.be',
+    url='http://www.pybrid.com/',
     packages=['hey_pgsqlexec', ],
     license='LICENSE.txt',
     description='Hey! PostgreSQL SQL Executable Object',
